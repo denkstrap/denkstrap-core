016e3229d2f791fd4269732a584e9f4da1ea78d6
Matthias Permien <matthias.permien@denkwerk.com>
2018-02-20 14:58:29 +0100

    Add tests for dispatcher/event

--- src/utils/event/dispatcher.test.ts
+++ src/utils/event/dispatcher.test.ts
@@ -1,12 +1,17 @@
 import { Dispatcher } from './dispatcher';
+import { default as EventSystem } from './index';
 
 const TEST_EVENT_NAME = 'testEventName';
-const TEST_EVENT_NAME_NS1 = 'testArg1Content.ns1';
-const TEST_EVENT_NAME_NS2 = 'testArg1Content.ns1.ns2';
-const TEST_EVENT2_NAME_NS1 = 'testArg2Content.ns1';
-const TEST_EVENT2_NAME_NS2 = 'testArg2Content.ns1.ns2';
+const TEST_EVENT_NAME_NS1 = 'testEventName.ns1';
+const TEST_EVENT_NAME_NS2 = 'testEventName.ns1.ns2';
 const TEST_ARG1_CONTENT = 'testArg1Content';
 const TEST_ARG2_CONTENT = 'testArg2Content';
+
+describe( 'EventSystem', () => {
+    test( 'to be an instance of Dispatcher', () => {
+        expect( EventSystem instanceof Dispatcher ).toBe( true );
+    } );
+} );
 
 describe( 'Dispatcher', () => {
 
@@ -16,9 +21,9 @@ describe( 'Dispatcher', () => {
             const dispatcher = new Dispatcher();
             const mockFunction = jest.fn();
 
-            dispatcher.on( TEST_EVENT_NAME, mockFunction);
+            dispatcher.on( TEST_EVENT_NAME, mockFunction );
 
-            dispatcher.trigger( TEST_EVENT_NAME  );
+            dispatcher.trigger( TEST_EVENT_NAME );
 
             expect( mockFunction.mock.calls.length ).toBe( 1 );
         } );
@@ -27,10 +32,10 @@ describe( 'Dispatcher', () => {
             const dispatcher = new Dispatcher();
             const mockFunction = jest.fn();
 
-            dispatcher.on( TEST_EVENT_NAME, mockFunction);
+            dispatcher.on( TEST_EVENT_NAME, mockFunction );
 
-            dispatcher.trigger( TEST_EVENT_NAME  );
-            dispatcher.trigger( TEST_EVENT_NAME  );
+            dispatcher.trigger( TEST_EVENT_NAME );
+            dispatcher.trigger( TEST_EVENT_NAME );
 
             expect( mockFunction.mock.calls.length ).toBe( 2 );
         } );
@@ -65,7 +70,7 @@ describe( 'Dispatcher', () => {
                 foo: 'bar'
             };
 
-            dispatcher.on( TEST_EVENT_NAME, function(this: any) {
+            dispatcher.on( TEST_EVENT_NAME, function( this: any ) {
                 expect( this ).toBe( context );
             } );
 
@@ -81,9 +86,9 @@ describe( 'Dispatcher', () => {
             const dispatcher = new Dispatcher();
             const mockFunction = jest.fn();
 
-            dispatcher.on( TEST_EVENT_NAME, mockFunction);
+            dispatcher.on( TEST_EVENT_NAME, mockFunction );
 
-            dispatcher.trigger( TEST_EVENT_NAME  );
+            dispatcher.trigger( TEST_EVENT_NAME );
 
             expect( mockFunction.mock.calls.length ).toBe( 1 );
         } );
@@ -91,30 +96,46 @@ describe( 'Dispatcher', () => {
         test( 'to subscribe and react multiple times to an event', () => {
             const dispatcher = new Dispatcher();
             const mockFunction = jest.fn();
-            dispatcher.on( TEST_EVENT_NAME, mockFunction);
+            dispatcher.on( TEST_EVENT_NAME, mockFunction );
 
-            dispatcher.trigger( TEST_EVENT_NAME  );
-            dispatcher.trigger( TEST_EVENT_NAME  );
+            dispatcher.trigger( TEST_EVENT_NAME );
+            dispatcher.trigger( TEST_EVENT_NAME );
 
             expect( mockFunction.mock.calls.length ).toBe( 2 );
         } );
 
-        test.skip( 'to subscribe an event with a namespace', () => {
+        test( 'to subscribe an event with a namespace', () => {
             const dispatcher = new Dispatcher();
             const mockFunction1 = jest.fn();
             const mockFunction2 = jest.fn();
 
-            dispatcher.on( TEST_EVENT_NAME, mockFunction1);
-            dispatcher.on( TEST_EVENT_NAME_NS1, mockFunction1);
+            dispatcher.on( TEST_EVENT_NAME, mockFunction1 );
+            dispatcher.on( TEST_EVENT_NAME_NS1, mockFunction2 );
 
-            dispatcher.trigger( TEST_EVENT_NAME  );
+            dispatcher.trigger( TEST_EVENT_NAME );
+            dispatcher.trigger( TEST_EVENT_NAME_NS1 );
 
             expect( mockFunction1.mock.calls.length ).toBe( 1 );
-            expect( mockFunction2.mock.calls.length ).toBe( 1 );
+            expect( mockFunction2.mock.calls.length ).toBe( 2 );
         } );
 
-        test.skip( 'to subscribe an event with multiple namespaces', () => {
-            //TODO
+        test( 'to subscribe an event with multiple namespaces', () => {
+            const dispatcher = new Dispatcher();
+            const mockFunction1 = jest.fn();
+            const mockFunction2 = jest.fn();
+            const mockFunction3 = jest.fn();
+
+            dispatcher.on( TEST_EVENT_NAME, mockFunction1 );
+            dispatcher.on( TEST_EVENT_NAME_NS1, mockFunction2 );
+            dispatcher.on( TEST_EVENT_NAME_NS2, mockFunction3 );
+
+            dispatcher.trigger( TEST_EVENT_NAME );
+            dispatcher.trigger( TEST_EVENT_NAME_NS1 );
+            dispatcher.trigger( TEST_EVENT_NAME_NS2 );
+
+            expect( mockFunction1.mock.calls.length ).toBe( 1 );
+            expect( mockFunction2.mock.calls.length ).toBe( 2 );
+            expect( mockFunction3.mock.calls.length ).toBe( 3 );
         } );
 
     } );
@@ -126,10 +147,10 @@ describe( 'Dispatcher', () => {
             const dispatcher = new Dispatcher();
             const mockFunction = jest.fn();
 
-            dispatcher.one( TEST_EVENT_NAME, mockFunction);
+            dispatcher.one( TEST_EVENT_NAME, mockFunction );
 
-            dispatcher.trigger( TEST_EVENT_NAME  );
-            dispatcher.trigger( TEST_EVENT_NAME  );
+            dispatcher.trigger( TEST_EVENT_NAME );
+            dispatcher.trigger( TEST_EVENT_NAME );
 
             expect( mockFunction.mock.calls.length ).toBe( 1 );
         } );
@@ -143,25 +164,75 @@ describe( 'Dispatcher', () => {
             const dispatcher = new Dispatcher();
             const mockFunction = jest.fn();
 
-            dispatcher.on( TEST_EVENT_NAME, mockFunction);
-            dispatcher.trigger( TEST_EVENT_NAME  );
+            dispatcher.on( TEST_EVENT_NAME, mockFunction );
+            dispatcher.trigger( TEST_EVENT_NAME );
 
             dispatcher.off( TEST_EVENT_NAME );
-            dispatcher.trigger( TEST_EVENT_NAME  );
+            dispatcher.trigger( TEST_EVENT_NAME );
 
             expect( mockFunction.mock.calls.length ).toBe( 1 );
         } );
 
-        test.skip( 'to unsubscribe an event with a namespace', () => {
-            //TODO
+        test( 'to unsubscribe an event that does not exist', () => {
+            const dispatcher = new Dispatcher();
+            dispatcher.off( TEST_EVENT_NAME );
         } );
 
-        test.skip( 'to unsubscribe an event with multiple namespace', () => {
-            //TODO
+        test( 'to unsubscribe an event with a namespace', () => {
+            const dispatcher = new Dispatcher();
+            const mockFunction1 = jest.fn();
+            const mockFunction2 = jest.fn();
+
+            dispatcher.on( TEST_EVENT_NAME, mockFunction1 );
+            dispatcher.on( TEST_EVENT_NAME_NS1, mockFunction2 );
+
+            dispatcher.trigger( TEST_EVENT_NAME );
+            dispatcher.off( TEST_EVENT_NAME_NS1 );
+
+            dispatcher.trigger( TEST_EVENT_NAME );
+
+            expect( mockFunction1.mock.calls.length ).toBe( 2 );
+            expect( mockFunction2.mock.calls.length ).toBe( 1 );
         } );
 
-        test.skip( 'to unsubscribe all events with and without namespaces', () => {
-            //TODO
+        test( 'to unsubscribe an event with multiple namespace', () => {
+            const dispatcher = new Dispatcher();
+            const mockFunction1 = jest.fn();
+            const mockFunction2 = jest.fn();
+            const mockFunction3 = jest.fn();
+
+            dispatcher.on( TEST_EVENT_NAME, mockFunction1 );
+            dispatcher.on( TEST_EVENT_NAME_NS1, mockFunction2 );
+            dispatcher.on( TEST_EVENT_NAME_NS2, mockFunction3 );
+
+            dispatcher.trigger( TEST_EVENT_NAME );
+            dispatcher.off( TEST_EVENT_NAME_NS2 );
+
+            dispatcher.trigger( TEST_EVENT_NAME );
+
+            expect( mockFunction1.mock.calls.length ).toBe( 2 );
+            expect( mockFunction2.mock.calls.length ).toBe( 2 );
+            expect( mockFunction3.mock.calls.length ).toBe( 1 );
+        } );
+
+        test( 'to unsubscribe all events with and without namespaces', () => {
+            const dispatcher = new Dispatcher();
+            const mockFunction1 = jest.fn();
+            const mockFunction2 = jest.fn();
+            const mockFunction3 = jest.fn();
+
+            dispatcher.on( TEST_EVENT_NAME, mockFunction1 );
+            dispatcher.on( TEST_EVENT_NAME_NS1, mockFunction2 );
+            dispatcher.on( TEST_EVENT_NAME_NS2, mockFunction3 );
+
+            dispatcher.trigger( TEST_EVENT_NAME );
+            dispatcher.off( TEST_EVENT_NAME );
+
+            dispatcher.trigger( TEST_EVENT_NAME );
+
+            expect( mockFunction1.mock.calls.length ).toBe( 1 );
+            expect( mockFunction2.mock.calls.length ).toBe( 1 );
+            expect( mockFunction3.mock.calls.length ).toBe( 1 );
         } );
 
     } );
